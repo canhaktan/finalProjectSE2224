c37b48fa2863186c5ba767fe756d7c631568eb7f
canhaktan <canhaktanunat@gmail.com>
2024-05-31 19:03:29 +0300

    Added DataValidationChecker class The class has isInteger(String txt) and isString(String txt) methods

---
+++ src/DataValidationChecker.java
@@ -0,0 +1,35 @@
+public class DataValidationChecker {
+    public boolean isInteger(String txt){
+        boolean integer = true;
+        for(int i = 0;i < txt.length();i++){
+            char c = txt.charAt(i);
+            int charInt = (int)c;
+            if(charInt < 48 || charInt > 57){
+                integer = false;
+                break;
+            }
+        }
+        return integer;
+    }
+
+    public boolean isString(String txt){
+        boolean string = true;
+        for(int i = 0;i < txt.length();i++){
+            if(!string){
+                break;
+            }
+            char c = txt.charAt(i);
+            int charInt = (int)c;
+            if((65 <= charInt && charInt <= 90) || (97 <= charInt && charInt <= 122)){
+                string = true;
+            }
+            else if(charInt != 32){//if not space char ASCII
+                string = false;
+            }
+        }
+        return string;
+    }
+
+
+
+}
