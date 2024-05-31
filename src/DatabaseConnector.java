10ecd630684734fe7c0cc75e3fdaa52effddb80d
canhaktan <canhaktanunat@gmail.com>
2024-05-31 14:36:58 +0300

    Database connection code in one single class

---
+++ src/DatabaseConnector.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class DatabaseConnector {
+    // I don't want to apply this connection manually all the time.
+    // I'm planning to work with only 1 database/schema which is 'finalproject'
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/finalproject";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
+    private Connection connection = null;
+    public Connection connect(){
+        if(connection == null){
+            try {
+                Class.forName(JDBC_DRIVER);
+                connection  = DriverManager.getConnection(DB_URL,USER,PASS);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
